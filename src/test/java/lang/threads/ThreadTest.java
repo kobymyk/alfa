4c675d94515054e989bd73bbe1ebac69f2b753a8
kobymyk <kobylkin@gmail.com>
2025-02-07 19:14:16 +0200

    ReentrantLock

--- src/test/java/lang/threads/ThreadTest.java
+++ src/test/java/lang/threads/ThreadTest.java
@@ -2,6 +2,10 @@ package lang.threads;
 
 import org.junit.Test;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThreadTest {
     @Test
     public void start() throws InterruptedException {
@@ -38,6 +42,7 @@ public class ThreadTest {
     public void waitNotify() throws InterruptedException {
         Thread t = new Thread() {
             int total;
+
             @Override
             public void run() {
                 synchronized (this) {
@@ -58,5 +63,78 @@ public class ThreadTest {
         synchronized (t) {
             t.wait();
         }
+    }
+
+    @Test
+    public void tryLock() {
+        Lock lock = new ReentrantLock();
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println(getName() + "start");
+                try {
+                    sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                lock.unlock();
+                System.out.println(getName() + "unlock");
+            }
+        };
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + "start");
+                while (true) {
+                    if (lock.tryLock()) {
+                        System.out.println(getName() + "work");
+                        break;
+                    } else {
+                        System.out.println(getName() + "wait");
+                    }
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+
+    @Test
+    public void conditionSignal() {
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println(getName() + "start");
+                try {
+                    sleep(10);
+                    condition.signal();
+                    System.out.println(getName() + "signal");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                lock.unlock();
+                System.out.println(getName() + "unlock");
+            }
+        };
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + "start");
+                lock.lock();
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(getName() + "work");
+                lock.unlock();
+            }
+        };
+        t1.start();
+        t2.start();
     }
 }
